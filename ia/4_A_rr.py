3ba09af7c1abd69f4576293a6dbab0a8d8c57d6d
hhhramires <henrique.ramires.rs@gmail.com>
2024-08-24 18:35:53 -0300

    ajuste

--- ia/4_A_rr.py
+++ ia/4_A_rr.py
@@ -1,49 +1,79 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
-import re
+from sklearn.model_selection import GridSearchCV
 
-def extract_years(filename: str):
-    # Usar expressão regular para encontrar todos os números no nome do arquivo
-    years = re.findall(r'\d{4}', filename)
 
-    # Converter para inteiros e retornar como uma lista de números
-    return [int(year) for year in years]
+# Função para formatar os valores como dinheiro
+def money_formatter(x, pos):
+    return f'R${x:,.2f}'  # Formato brasileiro, ajustando para R$ e duas casas decimais
 
-modelo = 'Modelo de Regressão Regularizados - Ridge Regression'
-arquivo_treino = 'train_2021_2022.csv'
-arquivo_teste = 'test_2023.csv'
+
+arquivo_treino = '2021_2022.csv'
+arquivo_validation = '2023.csv'
 
 # Carregando os dados de treino e teste
-train_df = pd.read_csv('dataset/real/' + arquivo_treino, sep=';')
-test_df = pd.read_csv('dataset/real/' + arquivo_teste, sep=';')
+train_df = pd.read_csv('dataset/entradas/' + arquivo_treino, sep=',')
+test_df = pd.read_csv('dataset/entradas/' + arquivo_validation, sep=',')
 
 # Separando as features (X) e o target (y)
 X_train = train_df[['week_of_month', 'month']]
 y_train = train_df['value']
-X_test = test_df[['week_of_month', 'month']]
-y_test = test_df['value']
+X_val = test_df[['week_of_month', 'month']]
+y_val = test_df['value']
 
-# Criando e treinando o modelo Ridge Regression
-model = Ridge(alpha=1.0)
-model.fit(X_train, y_train)
+# Definindo a grade de parâmetros para o GridSearchCV
+param_grid = {
+    'alpha': [0.01, 0.1, 1.0, 10.0, 100.0]
+}
 
-# Fazendo previsões para o ano de 2023
-y_pred = model.predict(X_test)
+# Configurando o GridSearchCV com Ridge Regression
+grid_search = GridSearchCV(
+    estimator=Ridge(),
+    param_grid=param_grid,
+    scoring='neg_mean_squared_error',
+    cv=5,
+    n_jobs=-1,
+    verbose=1
+)
+
+# Treinando o modelo usando GridSearchCV
+grid_search.fit(X_train, y_train)
+
+# Obtendo o melhor modelo
+best_model = grid_search.best_estimator_
+
+# Fazendo previsões no conjunto de validação
+y_pred = best_model.predict(X_val)
 
 # Calculando o erro quadrático médio
-mse = mean_squared_error(y_test, y_pred)
+mse = mean_squared_error(y_val, y_pred)
 print(f'Mean Squared Error: {mse}')
 
+cv_results = grid_search.cv_results_
+for mean_score, params in zip(cv_results['mean_test_score'], cv_results['params']):
+    print(f'Mean Score: {mean_score}, Params: {params}')
+
+# Imprimindo os melhores parâmetros encontrados
+best_params = grid_search.best_params_
+print(f'Best hyperparameters: {best_params}')
+
+# Criando rótulos personalizados para o eixo X no formato 'week_of_month-month'
+x_labels = [f"{week}-{month}" for week, month in zip(X_val['week_of_month'], X_val['month'])]
+
 # Plotando os resultados
-plt.figure(figsize=(10, 6))
-plt.plot(range(len(y_test)), y_test, label='Valores Reais 2023', marker='o')
-plt.plot(range(len(y_pred)), y_pred, label='Previsões 2023', marker='x')
-plt.title('Previsão de Gastos para 2023 - Ridge Regression')
-plt.xlabel('Período')
-plt.ylabel('Valor')
-plt.legend()
+plt.figure(figsize=(15, 7))
+plt.title('Entradas de Dinheiro', fontsize=16)
+plt.plot(x_labels, y_val, marker='o', label='Real', markersize=6, linewidth=2)
+plt.plot(x_labels, y_pred, marker='o', label='Predito', markersize=6, linewidth=2)
+plt.xlabel('Semana - Mês', fontsize=14)  # Legenda do eixo X com fonte maior
+plt.ylabel('Valor em Dinheiro', fontsize=14)  # Legenda do eixo Y com fonte maior
+plt.legend(fontsize=14)  # Aumenta o tamanho da fonte da legenda
+plt.gca().yaxis.set_major_formatter(
+    mtick.FuncFormatter(money_formatter))  # Aplicando o formatador de dinheiro ao eixo Y
 plt.grid(True)
+plt.yticks(fontsize=14)
+plt.xticks(rotation=90, fontsize=12)  # Rotaciona os rótulos do eixo X e ajusta o tamanho da fonte
 plt.show()
