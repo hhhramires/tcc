a26bc90b30ed9e2d8e313f47fd1fa71e158ec624
hhhramires <henrique.ramires.rs@gmail.com>
2024-08-24 18:22:53 -0300

    ajuste

--- ia/2_A_dtr_train.py
+++ ia/2_A_dtr_train.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import pandas as pd
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeRegressor
 
 
@@ -13,7 +14,7 @@ def money_formatter(x, pos):
 arquivo_treino = '2021_2022.csv'
 arquivo_validation = '2023.csv'
 
-# Carregando os dados de treino e teste
+# Carregando os dados de treino e validação
 train_df = pd.read_csv('dataset/entradas/' + arquivo_treino, sep=',')
 validation_df = pd.read_csv('dataset/entradas/' + arquivo_validation, sep=',')
 
@@ -23,16 +24,44 @@ y_train = train_df['value']
 X_val = validation_df[['week_of_month', 'month']]
 y_val = validation_df['value']
 
-# Criando e treinando o modelo Decision Tree Regressor
-model = DecisionTreeRegressor(random_state=42)
-model.fit(X_train, y_train)
+# Definindo a grade de parâmetros para o GridSearchCV
+param_grid = {
+    'max_depth': [3, 5, 10, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': [None, 'auto', 'sqrt', 'log2']
+}
 
-# Fazendo previsões para o ano de 2023
-y_pred = model.predict(X_val)
+# Configurando o GridSearchCV com DecisionTreeRegressor
+grid_search = GridSearchCV(
+    estimator=DecisionTreeRegressor(random_state=42),
+    param_grid=param_grid,
+    scoring='neg_mean_squared_error',
+    cv=5,
+    n_jobs=-1,
+    verbose=1
+)
+
+# Treinando o modelo usando GridSearchCV
+grid_search.fit(X_train, y_train)
+
+# Obtendo o melhor modelo
+best_model = grid_search.best_estimator_
+
+# Fazendo previsões no conjunto de validação
+y_pred = best_model.predict(X_val)
 
 # Calculando o erro quadrático médio
 mse = mean_squared_error(y_val, y_pred)
 print(f'Mean Squared Error: {mse}')
+
+cv_results = grid_search.cv_results_
+for mean_score, params in zip(cv_results['mean_test_score'], cv_results['params']):
+    print(f'Mean Score: {mean_score}, Params: {params}')
+
+# Imprimindo os melhores parâmetros encontrados
+best_params = grid_search.best_params_
+print(f'Best hyperparameters: {best_params}')
 
 # Criando rótulos personalizados para o eixo X no formato 'week_of_month-month'
 x_labels = [f"{week}-{month}" for week, month in zip(X_val['week_of_month'], X_val['month'])]
