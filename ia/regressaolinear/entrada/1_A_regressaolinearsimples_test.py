39978b5c2afd914f576527425c57483127949c46
hhhramires <henrique.ramires.rs@gmail.com>
2024-08-25 17:19:44 -0300

    ajuste

---
+++ ia/regressaolinear/entrada/1_A_regressaolinearsimples_test.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+
+
+# Função para formatar os valores como dinheiro
+def money_formatter(x, pos):
+    return f'R${x:,.2f}'  # Formato brasileiro, ajustando para R$ e duas casas decimais
+
+
+arquivo_treino = '2021_2022.csv'
+arquivo_test = '2024.csv'
+
+# Carregando os dados de treino e teste
+train_df = pd.read_csv('../../dataset/entradas/' + arquivo_treino, sep=',')
+test_df = pd.read_csv('../../dataset/entradas/' + arquivo_test, sep=',')
+
+# Separando as features (X) e o target (y)
+X_train = train_df[['week_of_month', 'month']]
+y_train = train_df['value']
+X_val = test_df[['week_of_month', 'month']]
+y_val = test_df['value']
+
+# Criando um pipeline com PolynomialFeatures (grau fixo em 2) e LinearRegression
+pipeline = Pipeline([
+    ('poly', PolynomialFeatures(degree=2)),
+    ('scaler', StandardScaler()),
+    ('lin_reg', LinearRegression())
+])
+
+# Treinando o modelo com o conjunto de treino
+pipeline.fit(X_train, y_train)
+
+# Fazendo previsões para o ano de 2023
+y_pred = pipeline.predict(X_val)
+
+# Calculando o erro quadrático médio
+mse = mean_squared_error(y_val, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Criando rótulos personalizados para o eixo X no formato 'week_of_month-month'
+x_labels = [f"{week}-{month}" for week, month in zip(X_val['week_of_month'], X_val['month'])]
+
+# Plotando os resultados
+plt.figure(figsize=(15, 9))
+plt.title('Entradas de Dinheiro - treino 2021 e 2022, teste 01/2024 a 05/2024', fontsize=20)
+plt.plot(x_labels, y_val, marker='o', label='Real', markersize=6, linewidth=2)
+plt.plot(x_labels, y_pred, marker='o', label='Predito', markersize=6, linewidth=2)
+plt.xlabel('Semana - Mês', fontsize=20)
+plt.ylabel('Valor em Dinheiro', fontsize=20)
+plt.legend(fontsize=20)
+plt.gca().yaxis.set_major_formatter(mtick.FuncFormatter(money_formatter))
+plt.grid(True)
+plt.yticks(fontsize=14)
+plt.xticks(rotation=90, fontsize=14)
+plt.show()
--- ia/regressaolinear/entrada/1_A_regressaolinearsimples_train.py
+++ ia/regressaolinear/entrada/1_A_regressaolinearsimples_train.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 
 # Função para formatar os valores como dinheiro
@@ -14,37 +16,42 @@ def money_formatter(x, pos):
 arquivo_treino = '2021_2022.csv'
 arquivo_validation = '2023.csv'
 
-# Carregando os dados de treino e teste
-train_df = pd.read_csv('dataset/entradas/' + arquivo_treino, sep=',')
-test_df = pd.read_csv('dataset/entradas/' + arquivo_validation, sep=',')
+# Carregando os dados de treino e validacao
+train_df = pd.read_csv('../../dataset/entradas/' + arquivo_treino, sep=',')
+validation_df = pd.read_csv('../../dataset/entradas/' + arquivo_validation, sep=',')
 
 # Separando as features (X) e o target (y)
 X_train = train_df[['week_of_month', 'month']]
 y_train = train_df['value']
-X_val = test_df[['week_of_month', 'month']]
-y_val = test_df['value']
+X_val = validation_df[['week_of_month', 'month']]
+y_val = validation_df['value']
 
-# Definindo os parâmetros para a busca
+# Criando um pipeline com PolynomialFeatures e LinearRegression
+pipeline = Pipeline([
+    ('poly', PolynomialFeatures()),
+    ('scaler', StandardScaler()),
+    ('lin_reg', LinearRegression())
+])
+
+# Definindo os parâmetros para o GridSearchCV
 param_grid = {
-    'n_estimators': [100, 200, 300],
-    'learning_rate': [0.01, 0.1, 0.2],
-    'max_depth': [3, 5, 7],
-    'min_samples_split': [2, 5],
-    'min_samples_leaf': [1, 3]}
+    'poly__degree': [1, 2, 3, 4]
+}
 
-# Usando GridSearchCV para encontrar os melhores parâmetros
+# Configurando o GridSearchCV
 grid_search = GridSearchCV(
-    estimator=GradientBoostingRegressor(random_state=42),
+    estimator=pipeline,
     param_grid=param_grid,
-    scoring='neg_mean_squared_error',
     cv=5,
+    scoring='neg_mean_squared_error',
     n_jobs=-1,
     verbose=1)
 
-# Treinando o modelo
+# Treinando o modelo com GridSearchCV
 grid_search.fit(X_train, y_train)
 
-# Obtendo o melhor modelo
+# Melhores parâmetros e melhor modelo
+print("Best parameters:", grid_search.best_params_)
 best_model = grid_search.best_estimator_
 
 # Fazendo previsões para o ano de 2023
@@ -53,10 +60,10 @@ y_pred = best_model.predict(X_val)
 # Calculando o erro quadrático médio
 mse = mean_squared_error(y_val, y_pred)
 print(f'Mean Squared Error: {mse}')
-cv_results = grid_search.cv_results_
 
-# for mean_score, params in zip(cv_results['mean_test_score'], cv_results['params']):
-#    print(f'Mean Score: {mean_score}, Params: {params}')
+cv_results = grid_search.cv_results_
+for mean_score, params in zip(cv_results['mean_test_score'], cv_results['params']):
+    print(f'Mean Score: {mean_score}, Params: {params}')
 
 # Após o grid_search.fit(X_train, y_train)
 best_params = grid_search.best_params_
@@ -66,16 +73,15 @@ print(f'Best hyperparameters: {best_params}')
 x_labels = [f"{week}-{month}" for week, month in zip(X_val['week_of_month'], X_val['month'])]
 
 # Plotando os resultados
-plt.figure(figsize=(15, 7))
-plt.title('Entradas de Dinheiro', fontsize=16)
+plt.figure(figsize=(15, 9))
+plt.title('Entradas de Dinheiro - treino 2021 e 2022, validação 2023 ', fontsize=20)
 plt.plot(x_labels, y_val, marker='o', label='Real', markersize=6, linewidth=2)
 plt.plot(x_labels, y_pred, marker='o', label='Predito', markersize=6, linewidth=2)
-plt.xlabel('Semana - Mês', fontsize=14)  # Legenda do eixo X com fonte maior
-plt.ylabel('Valor em Dinheiro', fontsize=14)  # Legenda do eixo Y com fonte maior
-plt.legend(fontsize=14)  # Aumenta o tamanho da fonte da legenda
-plt.gca().yaxis.set_major_formatter(
-    mtick.FuncFormatter(money_formatter))  # Aplicando o formatador de dinheiro ao eixo Y
+plt.xlabel('Semana - Mês', fontsize=20)
+plt.ylabel('Valor em Dinheiro', fontsize=20)
+plt.legend(fontsize=20)
+plt.gca().yaxis.set_major_formatter(mtick.FuncFormatter(money_formatter))
 plt.grid(True)
 plt.yticks(fontsize=14)
-plt.xticks(rotation=90, fontsize=12)  # Rotaciona os rótulos do eixo X e ajusta o tamanho da fonte
+plt.xticks(rotation=90, fontsize=14)
 plt.show()
